939fa9583beb916ea63c9c882de207dfbd6ab763
Sergei <37253450+skendis@users.noreply.github.com>
2021-04-18 11:10:23 +0300

    Update firebase-messaging-sw.js

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -3,15 +3,15 @@ importScripts("https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js")
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDTWvnulv4pgV2SmmnyLAstNKBkR-PzlvU",
-  authDomain: "push-test-3784c.firebaseapp.com",
-  projectId: "push-test-3784c",
-  storageBucket: "push-test-3784c.appspot.com",
-  messagingSenderId: "922820243419",
-  appId: "1:922820243419:web:e4ac71c05a13ef396001fe",
-  measurementId: "G-R7JNNZJS3S"
-};
+  var firebaseConfig = {
+    apiKey: "AIzaSyDTWvnulv4pgV2SmmnyLAstNKBkR-PzlvU",
+    authDomain: "push-test-3784c.firebaseapp.com",
+    projectId: "push-test-3784c",
+    storageBucket: "push-test-3784c.appspot.com",
+    messagingSenderId: "922820243419",
+    appId: "1:922820243419:web:e4ac71c05a13ef396001fe",
+    measurementId: "G-R7JNNZJS3S"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 // Retrieve an instance of Firebase Messaging so that it can handle background
